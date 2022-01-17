18999ae7f8a803ae5aec2bb3e5f9650ff5d53111
Gokul A <79526422+gokulgopi31@users.noreply.github.com>
2022-01-17 18:30:19 +0530

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,14 @@
+﻿var countries = {
+    "CH": "Chennai",
+    "BE": "Bengaluru",
+    "KO": "Kolkota",
+    "MU": "Mumbai",
+    "PU": "Punjab",
+    "HY": "Hydrabed",
+    "PU": "Pune",
+    "GU": "Gujarat",
+    "TA": "Tamil nadu",
+    "TI": "Tiruppur",
+    "CO": "Coimbatore"
+   
+}
